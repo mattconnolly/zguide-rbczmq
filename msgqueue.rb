3b5b8d6f728bef44e12ff2ee2ed0096dc4f98eb0
Matt Connolly <matt.connolly@me.com>
2013-04-28 12:55:57 +1000

    adding msgqueue.rb

---
+++ msgqueue.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+require 'rbczmq'
+require_relative 'zmq-additions'
+
+context = ZMQ::Context.new
+
+frontend = context.socket(ZMQ::ROUTER)
+frontend.bind("tcp://*:5559")
+
+backend = context.socket(ZMQ::DEALER)
+backend.bind("tcp://*:5560")
+
+ZMQ.proxy(frontend, backend, nil)
---
+++ zmq-additions.rb
@@ -0,0 +1,29 @@
+module ZMQ
+  class ProxyHandler < Handler
+    def initialize(poll_item, socket_one, socket_two)
+      super
+      @socket_one = socket_one
+      @socket_two = socket_two
+    end
+
+    def on_readable
+      message = @socket_one.recv_message
+      @socket_two.send_message message
+    end
+  end
+
+  def self.proxy(frontend, backend, capture=nil)
+    # ideally, this should just call the ZMQ zmq_proxy function,
+    # but it appears to be missing from the interface. This is an incomplete
+    # implementation, it does nothing with the capture socket.
+    loop = Loop.new
+    puts loop.inspect
+    poll_front = Pollitem.new(frontend, ZMQ::POLLIN)
+    poll_back = Pollitem.new(backend, ZMQ::POLLIN)
+    poll_front.handler = ProxyHandler.new poll_front, frontend, backend
+    poll_back.handler = ProxyHandler.new poll_back, backend, frontend
+    loop.register(poll_front)
+    loop.register(poll_back)
+    loop.start
+  end
+end
