dab154df88a0ff45fef97b58303cb7ef6eae548e
Matt Connolly <matt.connolly@me.com>
2013-04-29 21:53:33 +1000

    fixed mtserver with ZMQ.proxy

--- mtserver.rb
+++ mtserver.rb
@@ -7,7 +7,6 @@ context = ZMQ::Context.new
 def worker(context)
   receiver = context.socket(ZMQ::REP)
   receiver.connect("inproc://workers")
-  receiver.verbose = true
   loop do
     string = receiver.recv
     puts "Receiver request: [#{string}]"
@@ -18,9 +17,7 @@ end
 
 clients = context.socket(ZMQ::ROUTER)
 clients.bind("tcp://*:5555")
-clients.verbose = true
 workers = context.socket(ZMQ::DEALER)
-workers.verbose = true
 workers.bind("inproc://workers")
 
 # create 5 workers
